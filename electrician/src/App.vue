b4410eea62b09693595447e980e05aa1f1609436
huang gao yuan <562453652@qq.com>
2021-01-13 15:58:08 +0800

    更新

--- electrician/src/App.vue
+++ electrician/src/App.vue
@@ -22,49 +22,6 @@ export default {
         this.isRouterAlive=true
       })
     },
-<<<<<<< HEAD
-     WebSocketTest(){
-         if(typeof(WebSocket) === "undefined"){
-                alert("您的浏览器不支持socket")
-            }else{
-                // 实例化socket
-                var uid = "123";
-                this.socket = new WebSocket(this.path+uid)
-                // 监听socket连接
-                this.socket.onopen = this.open
-                // 监听socket错误信息
-                this.socket.onerror = this.error
-                // 监听socket消息
-                this.socket.onmessage = this.getMessage
-//                 console.log(this.socket)
-            }
-    },
-    open: function () {
-            console.log("socket连接成功")
-        },
-    error: function () {
-        console.log("连接错误")
-    },
-    getMessage: function (msg) {   //content
-//         console.log(msg)
-        let obj=JSON.parse(msg.data) 
-        // console.log(obj.content)
-        this.messages=obj.content
-        // console.log(this.messages)
-          this.$dialog.alert({
-              width:"80%",
-              message: this.messages,
-              closeOnClickOverlay:true
-            });
-            },
-    send: function () {
-        this.socket.send(params)
-    },
-    close: function () {
-        console.log("socket已经关闭")
-    },
-=======
->>>>>>> 2d73917c7984921b77cf7051f267f9ccbbbd8b19
   },
   destroyed() {
     localStorage.clear()
