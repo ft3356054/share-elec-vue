2741729b8f01d2a4f3e4e6b4d7dfe6b3ccfedd24
Fuyanqi123 <48582127+Fuyanqi123@users.noreply.github.com>
2021-01-25 18:14:32 +0800

    修改bug

--- electrician/src/pages/electricianend/myorderabbing/index.vue
+++ electrician/src/pages/electricianend/myorderabbing/index.vue
@@ -60,21 +60,21 @@
                         <p>{{item1.voltage}} 抢修 {{item1.distance}}</p>
                     </dt>
                     <dd>
-                         <p v-show="item.orderElectricianStatus==='3'" style="color:red">人员增加</p>
-                         <p v-show="item.orderElectricianStatus==='4'" style="color:red">用户退单</p>
-                        <p v-show="item.orderElectricianStatus==='5'" style="color:red">已取消</p>
-                        <p v-show="item.orderElectricianStatus==='9'" style="color:red">已完成</p>
-                        <p v-show="item.orderElectricianStatus==='8'" style="color:red">待评价</p>
-                        <p v-show="item.orderElectricianStatus==='0'" style="color:red">待预约</p>
-                        <p v-show="item.orderElectricianStatus==='21'" style="color:red">待维修</p>
-                        <p v-show="item.orderElectricianStatus==='22'" style="color:red">待现场勘察</p>
-                        <p v-show="item.orderElectricianStatus==='23'" style="color:red">待支付维修费</p>
-                        <p v-show="item.orderElectricianStatus==='24'" style="color:red">施工完成</p>
-                        <p v-show="item.orderElectricianStatus==='25'" style="color:red">待验收</p>
-                        <p v-show="item.orderElectricianStatus==='26'" style="color:red">待填写维修费</p>
-                        <p v-show="item.orderElectricianStatus==='31'" style="color:red">施工中</p>
-                         <p v-show="item.orderElectricianStatus==='1'" style="color:red">拒接订单</p>
-                        <p v-show="item.orderElectricianStatus==='2'" style="line-height:0"><button @click="quxiao(item)">取消</button> <button class="jiedan" @click="jiedan(item)">接单</button></p>
+                         <p v-show="item1.orderElectricianStatus==='3'" style="color:red">人员增加</p>
+                         <p v-show="item1.orderElectricianStatus==='4'" style="color:red">用户退单</p>
+                        <p v-show="item1.orderElectricianStatus==='5'" style="color:red">已取消</p>
+                        <p v-show="item1.orderElectricianStatus==='9'" style="color:red">已完成</p>
+                        <p v-show="item1.orderElectricianStatus==='8'" style="color:red">待评价</p>
+                        <p v-show="item1.orderElectricianStatus==='0'" style="color:red">待预约</p>
+                        <p v-show="item1.orderElectricianStatus==='21'" style="color:red">待维修</p>
+                        <p v-show="item1.orderElectricianStatus==='22'" style="color:red">待现场勘察</p>
+                        <p v-show="item1.orderElectricianStatus==='23'" style="color:red">待支付维修费</p>
+                        <p v-show="item1.orderElectricianStatus==='24'" style="color:red">施工完成</p>
+                        <p v-show="item1.orderElectricianStatus==='25'" style="color:red">待验收</p>
+                        <p v-show="item1.orderElectricianStatus==='26'" style="color:red">待填写维修费</p>
+                        <p v-show="item1.orderElectricianStatus==='31'" style="color:red">施工中</p>
+                         <p v-show="item1.orderElectricianStatus==='1'" style="color:red">拒接订单</p>
+                        <p v-show="item1.orderElectricianStatus==='2'" style="line-height:0"><button @click="quxiao(item)">取消</button> <button class="jiedan" @click="jiedan(item1)">接单</button></p>
                     </dd>
                 </dl>
             </div>
